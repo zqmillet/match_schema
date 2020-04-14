95c3bcf56708fd5975ce3e47f1f20df9f7041d3e
zqmillet <zqmillet@qq.com>
2020-04-14 22:19:11 +0800

    [Test] create some testcases

--- testcases/base.py
+++ testcases/base.py
@@ -1,3 +1,25 @@
 from match_schema import match_schema
+from match_schema import MatchSchemaException
 
-import pdb; pdb.set_trace()
+def test1():
+    schema = '''
+    type: dict
+    '''
+    data = dict()
+
+    exception = match_schema(schema = schema, data = data)
+    assert exception is None
+
+def test2():
+    schema = '''
+    type: dict
+    '''
+
+    data = list()
+    exception = match_schema(schema = schema, data = data)
+
+    assert isinstance(exception, MatchSchemaException)
+
+if __name__ == '__main__':
+    test1()
+    test2()
