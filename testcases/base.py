ea1856d85f82d8a96ceca8d5c82528ce36f46ff5
zqmillet <zqmillet@qq.com>
2020-04-17 07:41:41 +0800

    [Test] add testcases

--- testcases/base.py
+++ testcases/base.py
@@ -14,17 +14,36 @@ def test2():
     schema = '''
     type: dict
     '''
-
     data = list()
-    exception = match_schema(schema = schema, data = data)
 
+    exception = match_schema(schema = schema, data = data)
     assert isinstance(exception, MatchSchemaException)
 
 def test3():
     schema = '''
     type: int
     '''
-
     data = 3
+
     exception = match_schema(schema = schema, data = data)
     assert exception is None
+
+def test4():
+    schema = '''
+    type: list
+    '''
+    data = [1, 2, 3]
+
+    exception = match_schema(schema = schema, data = data)
+    assert exception is None
+
+def test5():
+    schema = '''
+    type: list
+    items:
+        type: str
+    '''
+    data = [1, 2, 3]
+
+    exception = match_schema(schema = schema, data = data)
+    assert isinstance(exception, MatchSchemaException)
